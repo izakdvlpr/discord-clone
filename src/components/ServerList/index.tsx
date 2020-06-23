df8c206abffd9b33e1de395b0cfa23a4c8af9e79
zevdvlpr <zevdvlpr@gmail.com>
2020-06-23 12:40:18 -0300

    create layout and components

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import ServerButton from "../ServerButton";
+
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+      
+      <Separator />
+      
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton hasNotifications mentions={3} />
+      <ServerButton hasNotifications />      
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications mentions={30} />
+    </Container>
+  );
+}
+
+export default ServerList;
